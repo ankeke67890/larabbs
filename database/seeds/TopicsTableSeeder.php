22e08f35e150b6c15b8425daaf97805c909a0183
vagrant <vagrant@homestead>
2018-11-01 13:37:55 +0000

    seeding topics and users data

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,17 +2,35 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        //获取所有用户ID数组
+        $user_ids = User::all()->pluk('id')->toArray();
+
+        //获取所有帖子分类的Id数组
+        $category_ids = Category::all()->pluk('id')->toArray();
+
+        //获取faker实例
+        $faker = app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)
+                        ->time(100)
+                        ->make()
+                        ->each(function($topic, $index)
+                                use($user_ids, $category_ids, $faker)
+        {
+            //从id中随机取出一个并且赋值
+            $topic->user_id = $faker->randomElement($user_ids);
+
+            $topic->category_id = $faker->randomElement($category_ids);
         });
 
+        //将数据集合转换成数组并且写入数据库
         Topic::insert($topics->toArray());
     }
 
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //获取faker实例
+        $faker = app(Faker\Generator::class);
+        // 头像假数据
+        $avatars = [
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+
+        //生成数据集合
+        $users = factory(User::class)
+                        ->time(10)
+                        ->make()
+                        ->each(function($user, $index)
+                            use($faker, $avatars)
+                        {
+                            //从头像数组中随机取出一个并赋值
+                            $user->avatars = $faker->randomElement($avatars);
+
+                        });
+
+        //让隐藏数据可见，并将数据集合转换成数组
+        $user_array =$users->makeVisible(['password', 'remember_token'])->toArray();
+
+        //写入数据库
+        User::insert($user_array);
+
+        //单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'Summer';
+        $user->email = 'summer@yousails.com';
+        $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->save();
+
+    }
+}
