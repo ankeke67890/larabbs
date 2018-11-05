366cee4f0e1428c95acc811b09dff6cd387e271a
vagrant <vagrant@homestead>
2018-11-05 01:34:17 +0000

    User can create topic

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,3 +4,10 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+//定义添加文章模型的saving监视器辅助方法
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', '', strip_tags($value)));
+    return str_limit($excerpt,$length);
+}
